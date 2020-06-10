17b4680ebafaf08d123680bc10e8c717d9af12e0
namgivu <namgivu@gmail.com>
2020-06-10 21:09:24 +0700

    dockerize @ build+run

---
+++ docker/.config.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+IMAGE_NAME=nnh/dockerize_reactjs
+ CONTAINER=nnh_dockerize_reactjs
+      PORT=20611
---
+++ docker/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+SH=`cd $(dirname $BASH_SOURCE) && pwd`  # SH aka SCRIPT_HOME
+AH=`cd $SH/.. && pwd`  # AH aka APP_HOME
+
+source "$SH/.config.sh"
+    cd $AH
+        docker build   --file "$SH/Dockerfile"   -t $IMAGE_NAME      $AH
+        #              Docker file path          docker image name   source_folder
+    cd - 1>/dev/null
