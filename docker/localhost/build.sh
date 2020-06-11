3dd93eb981a78dfc7d2c3cf9c8f0b9c70a45e3b9
namgivu <namgivu@gmail.com>
2020-06-11 17:59:18 +0700

    we have multiple distro of .dockerignore ie it clone from docker/localhost/.dockerignore, and docker/production/.dockerignore --> we ignore its copied destination file

--- docker/localhost/build.sh
+++ docker/localhost/build.sh
@@ -4,6 +4,7 @@ AH=`cd $SH/../.. && pwd`  # AH aka APP_HOME
 
 source "$SH/.config.sh"
     cd $AH
-        docker build   --file "$SH/Dockerfile"   -t $IMAGE_NAME      $AH
-        #              Docker file path          docker image name   source_folder
+        cp -f "$SH/.dockerignore" "$AH/.dockerignore"  # create .dockerignore of this deploy-distro
+            docker build   --file "$SH/Dockerfile"   -t $IMAGE_NAME      $AH
+            #              Docker file path          docker image name   source_folder
     cd - 1>/dev/null
